717bc9f2dfe71de9076ae20ef0c586497eb02159
Topias Naskali <topias.naskali@lab.student.fi>
2023-09-12 12:27:22 +0300

    Product java luokka luotu

---
+++ src/main/java/com/white/shoppinglist/domain/Product.java
@@ -0,0 +1,74 @@
+package com.white.shoppinglist.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name, details;
+    private double price;
+    private int quantity;
+
+    public Product(long id, String name, String details, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.details = details;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
---
+++ src/main/java/com/white/shoppinglist/domain/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.white.shoppinglist.domain;
+
+public class ProductRepository {
+
+}

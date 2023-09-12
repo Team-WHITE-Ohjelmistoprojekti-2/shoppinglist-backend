9b7dd1df564b99912d297958f0cc1f215acf878b
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-09-12 13:12:23 +0300

    update rest controller

--- src/main/java/com/white/shoppinglist/web/ProductRestController.java
+++ src/main/java/com/white/shoppinglist/web/ProductRestController.java
@@ -1,27 +1,41 @@
 package com.white.shoppinglist.web;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Optional;
 import com.white.shoppinglist.domain.Product;
+import com.white.shoppinglist.domain.ProductRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class ProductRestController {
+    @Autowired
+    private ProductRepository productRepository;
     
     // Get all products.
-    // TODO: use product repository to get from database
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        return new ArrayList<Product>();
+        List<Product> products = (List<Product>)productRepository.findAll();
+
+        return products;
     }
 
     // Get product by id.
-    // TODO: use product repository to get from database
-    @GetMapping("/products/{id}")
-	public Product getProductById(@PathVariable("id") long productId) {
-		return new Product();
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Optional<Product>> getProductById(@PathVariable("id") Long productId) {
+		Optional<Product> product = productRepository.findById(productId);
+		HttpStatusCode status = HttpStatus.NOT_FOUND;
+		
+		if (product.isPresent()) {
+			status = HttpStatus.OK;
+		}
+		
+		return new ResponseEntity<>(product, status);
 	}
 }

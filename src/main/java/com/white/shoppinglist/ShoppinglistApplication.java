a0171e56744fa05c17b61db711b3ef039492fe99
Oliver Roman <oltsu5@hotmail.com>
2023-09-15 19:00:05 +0300

    front and back connected

--- src/main/java/com/white/shoppinglist/ShoppinglistApplication.java
+++ src/main/java/com/white/shoppinglist/ShoppinglistApplication.java
@@ -1,9 +1,15 @@
 package com.white.shoppinglist;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 import com.white.shoppinglist.domain.Product;
@@ -31,4 +37,23 @@ public class ShoppinglistApplication {
 		};
 	}
 
+	@Bean
+
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOriginPatterns(List.of("http://localhost:517*"));
+
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+
+	}
 }
--- src/main/java/com/white/shoppinglist/web/ProductRestController.java
+++ src/main/java/com/white/shoppinglist/web/ProductRestController.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 import com.white.shoppinglist.domain.Product;
 import com.white.shoppinglist.domain.ProductRepository;
 
+//import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,49 +20,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5174/")
 public class ProductRestController {
+    
+    //private static final Logger log = LoggerFactory.getLogger(ProductRestController.class);
     @Autowired
     private ProductRepository productRepository;
-    
+
     // Get all products.
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        List<Product> products = (List<Product>)productRepository.findAll();
+        List<Product> products = (List<Product>) productRepository.findAll();
 
         return products;
     }
 
     // Get product by id.
-	@GetMapping("/products/{id}")
-	public ResponseEntity<Optional<Product>> getProductById(@PathVariable("id") Long productId) {
-		Optional<Product> product = productRepository.findById(productId);
-		HttpStatusCode status = HttpStatus.NOT_FOUND;
-		
-		if (product.isPresent()) {
-			status = HttpStatus.OK;
-		}
-		
-		return new ResponseEntity<>(product, status);
-	}
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable("id") Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+        HttpStatusCode status = HttpStatus.NOT_FOUND;
+
+        if (product.isPresent()) {
+            status = HttpStatus.OK;
+        }
+
+        return new ResponseEntity<>(product, status);
+    }
 
     // post mapping, creates new product. SHOULD WORK
     @PostMapping("/addproduct")
-public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-    Product createdProduct = productRepository.save(product);
-    return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
-}
-
- // delete mapping, deletes product by id returns errorcode if product is not found.
-@DeleteMapping("/deleteproduct/{id}")
-public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long productId) {
-    Optional<Product> product = productRepository.findById(productId);
-    
-    if (product.isPresent()) {
-        productRepository.deleteById(productId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } else {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productRepository.save(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
-}
+
+    // delete mapping, deletes product by id returns errorcode if product is not
+    // found.
+    @DeleteMapping("/deleteproduct/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+
+        if (product.isPresent()) {
+            productRepository.deleteById(productId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 }

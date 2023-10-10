bd052f9903c9317901b212ec56e6807064d16103
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-10-10 14:54:10 +0300

    get shopping list by id

--- src/main/java/com/white/shoppinglist/web/ShoppingListRestController.java
+++ src/main/java/com/white/shoppinglist/web/ShoppingListRestController.java
@@ -1,8 +1,10 @@
 package com.white.shoppinglist.web;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.white.shoppinglist.EntityMapper;
+import com.white.shoppinglist.domain.Product;
 import com.white.shoppinglist.domain.ShoppingList;
 import com.white.shoppinglist.domain.ShoppingListRepository;
 import org.slf4j.LoggerFactory; //useful later
@@ -11,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping; //coming soon
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ShoppingListRestController {
     
     @Autowired
-    private ShoppingListRepository shoppinglistrepository;
+    private ShoppingListRepository shoppingListRepository;
 
     @Autowired
     private EntityMapper mapper;
@@ -30,7 +33,7 @@ public class ShoppingListRestController {
     // Get mapping, retrieves all shopping lists.
     @GetMapping("/shoppinglists")
     public ResponseEntity<List<ShoppingListDTO>> getAllShoppingLists() {
-        List<ShoppingList> shoppingLists = (List<ShoppingList>) shoppinglistrepository.findAll();
+        List<ShoppingList> shoppingLists = (List<ShoppingList>) shoppingListRepository.findAll();
 
         // Map shopping lists to DTOs
         List<ShoppingListDTO> shoppingListDTOs = shoppingLists
@@ -39,6 +42,18 @@ public class ShoppingListRestController {
             .collect(Collectors.toList());
 
         return new ResponseEntity<>(shoppingListDTOs, HttpStatus.OK);
+    }
+
+    // Retrieves shopping list by id.
+    @GetMapping("/shoppinglists/{id}")
+    public ResponseEntity<ShoppingListDTO> getShoppinglistById(@PathVariable("id") Long id) {
+        Optional<ShoppingList> shoppingList = shoppingListRepository.findById(id);
+
+        if (shoppingList.isPresent()) {
+            return new ResponseEntity<>(mapper.toDto(shoppingList.get()), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     // Post mapping, creates a new shopping list.
@@ -53,7 +68,7 @@ public class ShoppingListRestController {
         ShoppingList shoppingList = new ShoppingList(shoppingListDTO.getName());
 
         // Save the new shopping list
-        ShoppingList createdShoppingList = shoppinglistrepository.save(shoppingList);
+        ShoppingList createdShoppingList = shoppingListRepository.save(shoppingList);
 
         return new ResponseEntity<>(mapper.toDto(createdShoppingList), HttpStatus.CREATED);
     }

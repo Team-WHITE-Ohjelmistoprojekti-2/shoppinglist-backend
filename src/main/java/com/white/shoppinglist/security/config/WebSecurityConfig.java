53b82e557dccb4c7610503422d88a59c4c39fbe3
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-10-30 15:32:53 +0200

    initial WebSecurityConfig

--- src/main/java/com/white/shoppinglist/security/config/WebSecurityConfig.java
+++ src/main/java/com/white/shoppinglist/security/config/WebSecurityConfig.java
@@ -1,5 +1,64 @@
+// Based on https://github.com/amigoscode/login-registration-backend/issues/13
+
 package com.white.shoppinglist.security.config;
 
+import com.white.shoppinglist.appuser.AppUserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
-    // TODO
+
+    private final AppUserService appUserservice;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig(AppUserService appUserService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.appUserservice = appUserService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    // Cookie authentication might not work with REST API.
+    // Possibly needs token authentication with something like JSON Web Token.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Not final
+        RequestMatcher matcherRegistration = new AntPathRequestMatcher("/api/registration/**");
+
+        http
+            .authenticationProvider(daoAuthenticationProvider())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(matcherRegistration).permitAll()
+                .anyRequest().authenticated())
+            .formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserservice);
+        
+        return provider;
+    }
 }

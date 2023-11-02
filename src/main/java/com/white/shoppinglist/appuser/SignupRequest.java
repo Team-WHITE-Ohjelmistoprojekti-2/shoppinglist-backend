d5f19859728f0d78217930a8dc16cd9bea9435c9
DiviXe <ahopalo.leo@gmail.com>
2023-11-02 21:37:56 +0200

    Added password validation, One uppercase and one number required.

--- src/main/java/com/white/shoppinglist/appuser/SignupRequest.java
+++ src/main/java/com/white/shoppinglist/appuser/SignupRequest.java
@@ -1,6 +1,8 @@
 package com.white.shoppinglist.appuser;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class SignupRequest {
@@ -8,8 +10,11 @@ public class SignupRequest {
     @Size(min = 2, max = 20)
     private String username;
 
-    @NotBlank
+    @NotBlank //same as not empty?
+    @NotEmpty (message = "Password cannot be empty!")
     @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[0-9]).*$", message = "Password must contain at least one number")
+    @Pattern(regexp = "^(?=.*[A-Z]).*$", message = "Password must contain at least one uppercase letter")
     private String password;
     
     public SignupRequest(String username, String password) {

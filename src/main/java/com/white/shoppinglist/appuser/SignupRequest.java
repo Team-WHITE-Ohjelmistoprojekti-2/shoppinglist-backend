c878fcf314459f66854564a1bb4f75be05cc06f9
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-11-03 10:57:35 +0200

    user registration polished

--- src/main/java/com/white/shoppinglist/appuser/SignupRequest.java
+++ src/main/java/com/white/shoppinglist/appuser/SignupRequest.java
@@ -5,16 +5,18 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+// Right now RegistrationRequest.java is used for register.
 public class SignupRequest {
-    @NotBlank
-    @Size(min = 2, max = 20)
+    @NotBlank(message = "Username must contain at least one non-whitespace character")
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 2, max = 20, message = "Username must be between 2-20 characters")
     private String username;
 
-    @NotBlank //same as not empty?
-    @NotEmpty (message = "Password cannot be empty!")
-    @Size(min = 6)
+    @NotBlank(message = "Password must contain at least one non-whitespace character")
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must have at least 6 characters")
     @Pattern(regexp = "^(?=.*[0-9]).*$", message = "Password must contain at least one number")
-    @Pattern(regexp = "^(?=.*[A-Z]).*$", message = "Password must contain at least one uppercase letter")
+    @Pattern(regexp = "^(?=.*[A-Z]).*$", message = "Password must contain at least one uppercase letter between A-Z")
     private String password;
     
     public SignupRequest(String username, String password) {
--- src/main/java/com/white/shoppinglist/registration/RegistrationRequest.java
+++ src/main/java/com/white/shoppinglist/registration/RegistrationRequest.java
@@ -1,15 +1,21 @@
 package com.white.shoppinglist.registration;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class RegistrationRequest {
-    @NotBlank(message = "Username cannot be empty")
-    @Size(min = 2, max = 20, message = "Username must have 2 - 20 characters")
+    @NotBlank(message = "Username must contain at least one non-whitespace character")
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 2, max = 20, message = "Username must be between 2-20 characters")
     private String username = "";
 
-    @NotBlank(message = "Password cannot be empty")
+    @NotBlank(message = "Password must contain at least one non-whitespace character")
+    @NotEmpty(message = "Password cannot be empty")
     @Size(min = 6, message = "Password must have at least 6 characters")
+    @Pattern(regexp = "^(?=.*[0-9]).*$", message = "Password must contain at least one number")
+    @Pattern(regexp = "^(?=.*[A-Z]).*$", message = "Password must contain at least one uppercase letter between A-Z")
     private String password = "";
 
     private String description = "";
